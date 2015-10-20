68b04d9c87b9032015c11e87e3d67595ed24f91c
Terence <tchai@onesmuck.com>
2015-10-20 08:57:59 -0400

    Changed TBD004 to TBD005

--- jDcuVFrkPackage/src/jDcuVFrkPackage/DcuVFrkMain.java
+++ jDcuVFrkPackage/src/jDcuVFrkPackage/DcuVFrkMain.java
@@ -1170,7 +1170,7 @@ public class DcuVFrkMain extends javax.swing.JFrame {
         } // else // QuietMode                        
         //-------------------------------------------------------------------------------
 
-//TBD004
+//TBD005
 // Display both the Communication Window and Progress Window
 //GlobalVars.commDataFrame.setVisible(true);
 //GlobalVars.progressFrame.setVisible(true);
